02b82628becaaa140114ea7c43fe807c03250cc8
Chenhao Xue <xch927027@pku.edu.cn>
2024-11-06 10:55:00 +0800

    debug reshape

--- smoothquant/fake_quant.py
+++ smoothquant/fake_quant.py
@@ -26,12 +26,12 @@ def quantize_weight_per_tensor_absmax(w, n_bits=8):
 @torch.no_grad()
 def quantize_activation_per_token_absmax(t, n_bits=8):
     t_shape = t.shape
-    t.view(-1, t_shape[-1])
+    t = t.reshape(-1, t_shape[-1])
     scales = t.abs().max(dim=-1, keepdim=True)[0]
     q_max = 2 ** (n_bits - 1) - 1
     scales.clamp_(min=1e-5).div_(q_max)
     t.div_(scales).round_().mul_(scales)
-    t.view(*t_shape)
+    t = t.reshape(*t_shape)
     return t
 
 
